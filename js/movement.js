531c5d09b360baabdeb94ebdad3548f95387c60b
Lo <logankees@gmail.com>
2018-10-14 07:52:04 -0500

    adding images and js folders
    plz work

---
+++ js/movement.js
@@ -0,0 +1,16 @@
+$(document).on("mousemove", function (event) {
+	
+	$("img.lizard").css("width", event.pageX)
+	$("img.lizard").css("height", event.pageY)
+
+	$("img.people").css("width"), $(window).width() - event.pageX)
+	$("img.people").css("height", event.pageY)
+
+	$("img.coming").css("width"), event.pageX)
+	$("img.coming").css("height"), #(window).height() - event.pageY)
+
+	$("img.soon").css("width"), $(window).width() - event.pageX)
+	$("img.soon").css("height", $(window).height() - event.pageY)
+
+
+})
