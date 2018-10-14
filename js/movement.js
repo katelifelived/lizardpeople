be860cb6251d0e0450cdad0e4cf4b1df2d903be1
Lo <logankees@gmail.com>
2018-10-14 07:56:14 -0500

    typo

--- js/movement.js
+++ js/movement.js
@@ -7,7 +7,7 @@ $(document).on("mousemove", function (event) {
 	$("img.people").css("height", event.pageY)
 
 	$("img.coming").css("width"), event.pageX)
-	$("img.coming").css("height"), #(window).height() - event.pageY)
+	$("img.coming").css("height"), $(window).height() - event.pageY)
 
 	$("img.soon").css("width"), $(window).width() - event.pageX)
 	$("img.soon").css("height", $(window).height() - event.pageY)
